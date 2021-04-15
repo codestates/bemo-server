f2d0df3fd52e52b99d8d60cc4f001f0c43967984
Jiyoung Moon <jiyoungmoon@Jiyoungui-MacBookPro.local>
2021-04-15 17:38:51 +0900

    feature2

--- controllers/users/signup.js
+++ controllers/users/signup.js
@@ -1,2 +1,35 @@
-const { user } = require("../models");
+const { User } = require("../../models");
 
+module.exports = async (req, res) => {
+    // TODO : 회원가입 로직 및 유저 생성 로직 작성
+    // 회원가입에 필요한 username, email, password, mobile 을 구조분해 할당으로 req.body에서 선언함
+    const { username, email, password } = req.body;
+    // 한개라도 일치하지 않는다면 422상태와 'insufficient parameters supplied' 메세지를 보냄
+    if (!username || !email || !password) {
+        res.status(422).send('회원정보를 정확히 입력해주세요.')
+    }
+    //findOrCreate - 특정 요소를 검색하거나, 존재하지 않으면 새로 생성
+    //findOrCreate 메서드는 DB에 특정 요소가 존재하는지 검사합니다. 
+    //만약 존재한다면 해당하는 인스턴스를 반환하고, 그렇지 않으면 새로 생성합니다.
+    // 새로 생성하면 true, 생성하지 않으면 false 반환
+    User.findOrCreate({
+        where: {
+            email: email,
+        },
+        defaults: {
+            username: username,
+            password: password,
+        }
+    })
+        // findOrCreate => [user값, boolean] 배열형태 
+        .then(([user, created]) => {
+            // false 라면 409상태와 'email exists'
+            // console.log(user)
+            // false, email 존재한다면
+            if (!created) {
+                res.status(409).send('이메일 또는 사용자이름이 이미 존재합니다.');
+            }
+            // true, email 존재하지 않으면
+            res.status(201).send(user);
+        });
+}
