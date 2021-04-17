279cf9d86b9300a4aeeab1b0034ffd4970475282
Jiyoung Moon <jiyoungmoon@Jiyoungui-MacBookPro.local>
2021-04-17 22:03:12 +0900

    feature/7/withdrawal

---
+++ controllers/users/withdrawal.js
@@ -0,0 +1,23 @@
+const { User } = require("../../models");
+const jwt = require('jsonwebtoken');
+
+module.exports = async (req, res) => {
+    const authorization = req.headers.authorization;
+    if (authorization === undefined) {
+        res.status(401).json({
+            error: 'not authorized'
+        })
+    }
+    else {
+        const token = authorization.split(' ')[1]
+        const data = jwt.verify(token, process.env.ACCESS_SECRET)
+
+        await User.destroy({
+            where: { id: data.id }
+        })
+        res.clearCookie('refreshToken');
+        req.headers.authorization = null
+
+        res.status(200).json({ "message": "회원탈퇴 되었습니다." })
+    }
+}
