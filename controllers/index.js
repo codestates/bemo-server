751963eeace337d5a5df612dafb941fad091a57d
Jiyoung Moon <jiyoungmoon@Jiyoungui-MacBookPro.local>
2021-04-18 16:04:52 +0900

    modified

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,6 @@ module.exports = {
     accessToken: require('./users/accessToken'),
     refreshToken: require('./users/refreshToken'),
     signup: require('./users/signup'),
-    logout: require('./users/logout'),
     mypage: require('./users/mypage'),
     withdrawal: require('./users/withdrawal'),
     google: require('./users/google')
